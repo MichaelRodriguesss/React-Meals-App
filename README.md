059c32eccb7245a55da2c19e8d2e5350b59eff8b
Michael <michaeledavi12@gmail.com>
2022-04-27 22:25:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Meals
+
+---
+
+## Language and Tools
+
+- HTML
+- CSS
+- JavaScript
+
+---
+
+# App of an Restaurant that add new orders and shows it in the Menu and it also delete it
+
+<h1 align="center"> 
+    <img src="" />
+  </h2>
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicios food !" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
